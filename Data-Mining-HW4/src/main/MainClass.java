c24cdb70c9b72248260b6f643c4904bb25571656
linuxguy467 <dpplrdr435@gmail.com>
2017-04-19 23:11:16 -0400

    Added Main Class

---
+++ Data-Mining-HW4/src/main/MainClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author mchem
+ *
+ */
+public class MainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Data-Mining-HW4/src/main/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author mchem
+ *
+ */
+package main;
